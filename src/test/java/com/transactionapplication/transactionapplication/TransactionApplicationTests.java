abe015ae54ef26aa9b681b1ee366a62eb56df174
c0desun <lucase2040@gmail.com>
2022-12-20 00:33:30 -0300

    change class namse

--- src/test/java/com/transactionapplication/transactionapplication/TransactionApplicationTests.java
+++ src/test/java/com/transactionapplication/transactionapplication/TransactionApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PagarmeApplicationTests {
+class TransactionApplicationTests {
 
 	@Test
 	void contextLoads() {
